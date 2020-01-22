62f306e96b1c84e65390baf3f954a92ce840ddd9
AJ Meyer <ashley.meyer@gmail.com>
2020-01-21 22:09:11 -0600

    basic studio done

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. " +
+                "Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. " +
+                "Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Paragraph = Paragraph.ToLower();
+
+            Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+           for (int i=0; i<Paragraph.Length; i++)
+            {
+                if (!charCounts.ContainsKey(Paragraph[i]))
+                {
+                    charCounts.Add(Paragraph[i], 1);
+                }
+                else
+                {
+                    charCounts[Paragraph[i]] += 1;
+                }
+            }
+
+
+            foreach (char dictKey in charCounts.Keys)
+            {
+                Console.WriteLine(dictKey +" : " +charCounts[dictKey]);
+            }  
+        }
+    }
+}
